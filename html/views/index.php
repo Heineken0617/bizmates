e7b25effeec0c7f9d73abba3b6188e6a7469f1a0
Mark Sarmiento <marksarmiento0617@yahoo.com>
2021-03-24 23:09:39 +0800

    [INITIAL COMMIT]

---
+++ html/views/index.php
@@ -0,0 +1,34 @@
+<?php
+include_once('header.php');
+?>
+<div class="block">
+    <?php
+    include_once('navbar.php');
+    ?>
+    <br>
+    <div class="container-fluid">
+        <div class="row ml-3" style="color:#FFF8E5"> 
+            <div class="col-md-6 mt-5">
+            <h1 style="letter-spacing: 0.09em; font-size:2.5vw"> Welcome to</h1>
+            <h1 style="letter-spacing: 0.1em; font-size:6vw; font-weight:bolder"> JAPAN</h1>
+            <div class="row">
+                <div class="col-10">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus saepe esse nisi facilis animi et quo pariatur ullam commodi eaque accusantium totam perspiciatis ad laboriosam temporibus a debitis assumenda in, minus iste fugiat, quia ut iure dolorem! Veritatis aspernatur facilis, id, odit fugiat dicta voluptate ad, corrupti ducimus modi numquam!
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-5">
+                    <button class="btn btn-danger btn-md pl-4 pr-4" style="border-radius:50px;">
+                    Learn More
+                    </button>
+                </div>
+            </div>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+  
+</div>
+<?php
+include_once('footer.php');
+?>
---
+++ html/views/navbar.php
@@ -0,0 +1,26 @@
+<div class="container-fluid mt-3">
+<div class="row m-4">
+    <div class="col-md-5">
+     <strong>TRAVEL</strong>
+     GUIDE
+    </div>
+    <div class="col-md-7">
+        <div class="row ">
+            <div class="col-md-12 " style="text-align:right">
+                <a>
+                    HOME
+                </a>
+                <a>
+                    DESTINATION
+                </a>
+           
+                <a >
+                    WEATHER CONDITION
+                </a>
+
+        </div>
+    </div>
+</div>
+<hr>
+</div>
+</div>
